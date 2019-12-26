371787fc9d1a44c52c09a1d1827ea55e38d12332
Daniel Zigler <daniel.zsh@gmail.com>
2019-12-26 13:55:23 +0200

    ServerSide [Tutorial Stage]: added Redirect to Homepage Usage

--- ServerSide/MultiLive/MultiLive/urls.py
+++ ServerSide/MultiLive/MultiLive/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
+from articles import views as article_views
 
 from . import views
 
@@ -26,7 +28,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('about/', views.about),
-    path('', views.homepage),
+    path('', article_views.article_list, name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
