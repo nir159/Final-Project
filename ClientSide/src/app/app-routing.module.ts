ad914abe4ce19a4ffde249142867f10596b55616
nir159 <nir1125111@gmail.com>
2020-01-22 07:57:33 +0200

    tutorial p1

--- ClientSide/src/app/app-routing.module.ts
+++ ClientSide/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ContentChild } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ClientsComponent } from './clients/clients.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContentComponent } from './content/content.component';
+import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './header/header.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HeaderComponent},
+  {path: 'about', component: IntroComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'services', component: ContentComponent},
+  {path: 'testimonials', component: TestimonialComponent},
+  {path: 'clients', component: ClientsComponent},
+  {path: 'pricing', component: PricingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
