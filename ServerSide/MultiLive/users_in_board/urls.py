82e5e9408ab09a4b71516f651c70c92f14278dcd
Daniel Zigler <daniel.zsh@gmail.com>
2020-03-08 16:56:43 +0200

    Added users_in_board functionality

--- ServerSide/MultiLive/users_in_board/urls.py
+++ ServerSide/MultiLive/users_in_board/urls.py
@@ -10,4 +10,7 @@ router.register(r'users_in_board', views.UserInBoardViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework_users_in_board')),
+    path('get_users_in_board/', views.UsersInBoardAPIView.as_view()),
+    path('get_boards__user/', views.BoardsOfUserPIView.as_view()),
+    path('get_boards_of_user/<str:usr_>', views.get_user_boards),
 ]
--- ServerSide/MultiLive/users_in_board/views.py
+++ ServerSide/MultiLive/users_in_board/views.py
@@ -1,5 +1,7 @@
+import requests
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics, filters
 from .serializers import UserInBoardSerializer
 from .models import UserInBoard
 
@@ -14,3 +16,27 @@ class UserInBoardViewSet(viewsets.ModelViewSet):
         serializer = UserInBoardSerializer(my_users, many=True)
         return Response(serializer.data)'''
 
+
+class UsersInBoardAPIView(generics.ListCreateAPIView):
+    search_fields = ['board__id']
+    filter_backends = (filters.SearchFilter,)
+    queryset = UserInBoard.objects.all()
+    serializer_class = UserInBoardSerializer
+
+
+class BoardsOfUserPIView(generics.ListCreateAPIView):
+    search_fields = ['user__email']
+    filter_backends = (filters.SearchFilter,)
+    queryset = UserInBoard.objects.all()
+    serializer_class = UserInBoardSerializer
+
+
+def get_user_boards(request, usr_):
+
+    usr_boards = requests.get('http://localhost:8000/users_in_board/get_boards__user/?search={}'.format(usr_)).json()
+    boards_ids = []
+    for i in usr_boards:
+        boards_ids.append(i["board"])
+    print(boards_ids)
+
+    return HttpResponse(JsonResponse({'ids': boards_ids}))
